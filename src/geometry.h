f0db4fa98c4f8e04c4f586cb50812c34b45fc6b8
Leonid <reon.sem@gmail.com>
2019-05-23 14:04:10 -0400

    Edit geometry.h

---
+++ src/geometry.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+const float p = 3.14159265;
+extern float getarea(float r);
+extern float getperim(float r);
