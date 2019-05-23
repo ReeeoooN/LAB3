e9ec0231ecbfe17c492171e59f4377d39e3a7043
Leonid <reon.sem@gmail.com>
2019-05-23 14:22:36 -0400

    Edit test files in test

---
+++ test/TESTgetarea.cpp
@@ -0,0 +1,13 @@
+#include "TESTgeometry.h"
+float getarea(float r)
+{
+    float pl;
+    pl = p * r * r;
+    return pl;
+}
+
+TEST_CASE("GETAREA", "[AREA]") {
+  REQUIRE(getarea(2.2) == 15.20531f);
+  REQUIRE(getarea(6.11) == 117.28226f);
+  cout << "Test getarea is done" << endl;
+}
